3f1f1762a2c485c09533446a8bbe4f7e88e4c45c
Fernando Erivan <fernando.devcode@gmail.com>
2026-08-13 17:24:01 -0300

    Circulo adicionado ao projeto

---
+++ Aula 2/Circulo.java
@@ -0,0 +1,16 @@
+public class Circulo{
+    int raio;
+
+    Circulo(int raio){
+        this.raio = raio;
+    }
+
+    double area(){
+        return 3.14 * this.raio * this.raio;
+    }
+
+    double comprimento(){
+        return 2 * 3.14 * this.raio;
+        
+    }
+}
---
+++ Aula 2/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Circulo c1 = new Circulo(2);
+        Circulo c2 = new Circulo(3);
+
+            System.out.println("Área do círculo 1 é: "+ c1.area());
+            System.out.println("Comprimento de círculo 1 é: "+ c1.comprimento());
+            System.out.println("=========================================");
+            System.out.println("Área do círculo 2 é: "+ c2.area());
+            System.out.println("Comprimento de círculo 2 é: "+ c2.comprimento());
+        
+    }
+}
