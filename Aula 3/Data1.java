cc5f96b51ae53615c6b459b5a7a4aa2e24214c4e
Fernando Erivan <fernandoo.estudos@gmail.com>
2026-08-15 18:24:27 -0300

    Adicionado data e hora na aula 3

---
+++ Aula 3/Data1.java
@@ -0,0 +1,14 @@
+public class Data1 {
+    int dia;
+    int mes;
+    int ano;
+    Hora hora;
+
+    Data1(int dia, int mes, int ano, Hora hora){
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+        this.hora = hora;
+    }
+    
+}
---
+++ Aula 3/Hora.java
@@ -0,0 +1,11 @@
+public class Hora {
+    int hora;
+    int minutos;
+    int segundos;
+
+    Hora(int hora, int minutos, int segundos){
+        this.hora = hora;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+}
